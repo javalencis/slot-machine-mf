4703cc208f89125655e8bf6d4d8f9a47df4520d2
Jonathan Valencia <javalencis@gmail.com>
2024-05-06 14:30:05 -0500

    Mensajes de cupones

--- src/main.js
+++ src/main.js
@@ -1,6 +1,7 @@
 import { CharacterBot } from "./CharacterBot.js"
 import { CharacterMid } from "./CharacterMid.js"
 import { CharacterTop } from "./CharacterTop.js"
+import { msn } from "./msnCupon.js"
 
 
 const canvas = document.getElementById('myCanvas')
@@ -8,11 +9,13 @@ const ctx = canvas.getContext('2d')
 
 
 const order = ['donald', 'mickey', 'minnie', 'pluto']
-const orderMid = [ 'pluto', 'donald', 'mickey', 'minnie']
+const orderMid = ['pluto', 'donald', 'mickey', 'minnie']
 const btStart = document.querySelector('.bt-start')
 const btStop = document.querySelector('.bt-stop')
-
-const bgCanvas =  new Image()
+const contModal = document.querySelector('.container-modal')
+const contentMsn = document.querySelector('.content-msn')
+const contentCupon = document.querySelector('.content-cupon')
+const bgCanvas = new Image()
 bgCanvas.src = '../assets/bgcontainer.png'
 
 
@@ -31,13 +34,13 @@ const stateGames = {
     stopTop: false,
     stopMid: false,
     stopBot: false,
-
+    end: false
 }
 
 function start() {
-    pjsTop.forEach(pj => pj.setVelX(10))
-    pjsMid.forEach(pj => pj.setVelX(12))
-    pjsBot.forEach(pj => pj.setVelX(8))
+    pjsTop.forEach(pj => pj.setVelX(8))
+    pjsMid.forEach(pj => pj.setVelX(8))
+    pjsBot.forEach(pj => pj.setVelX(7))
     stateGames.start = false;
     stateGames.play = true;
     btStop.style.display = 'block'
@@ -114,11 +117,11 @@ function rePosition() {
 }
 
 function paired(index, list) {
-    if (index === 5) {
+    if (index === 4) {
         index = 0
     }
     if (index === -1) {
-        index = 4
+        index = 3
     }
     console.log('1');
     pairedPjs.push(list[index])
@@ -133,7 +136,20 @@ function checkPairing(listPjs, listOrder) {
         )))
 }
 
+function isEqual() {
+    if (pairedPjs.length !== 3) {
+        return false;
+    }
+
+    if (pairedPjs[0] === pairedPjs[1] && pairedPjs[1] === pairedPjs[2]) {
+        return true;
+    } else {
+        return false;
+    }
+}
+
 function manageStatesGame() {
+
     if (stateGames.start) {
         createPjs();
     }
@@ -158,11 +174,39 @@ function manageStatesGame() {
         if (CharacterBot.stopMoveBot) {
             checkPairing(pjsBot, order)
             stateGames.stopBot = false;
+            setTimeout(() => {
+
+                stateGames.end = true;
+            }, 1500);
+        }
+    }
+
+    if (stateGames.end) {
+        contModal.style.display = 'flex'
+    
+        if (isEqual()) {
+            if (pairedPjs[0] === 'mickey') {
+                contentMsn.innerHTML = msn.mickey.mensaje
+                contentCupon.innerHTML = msn.mickey.cupon
+            } else if (pairedPjs[0] === 'minnie') {
+                contentMsn.innerHTML = msn.minnie.mensaje
+                contentCupon.innerHTML = msn.minnie.cupon
+            } else if (pairedPjs[0] === 'donald') {
+                contentMsn.innerHTML = msn.donald.mensaje
+                contentCupon.innerHTML = msn.donald.cupon
+            } else {
+                contentMsn.innerHTML = msn.pluto.mensaje
+                contentCupon.style.display='none'
+            }
+        } else {
+            contentMsn.innerHTML = msn.nothing.mensaje
+            contentCupon.style.display='none'
         }
     }
 }
-function drawBg(){
-    ctx.drawImage(bgCanvas, 0, 0,canvas.width,canvas.height);
+
+function drawBg() {
+    ctx.drawImage(bgCanvas, 0, 0, canvas.width, canvas.height);
 }
 
 function draw() {
---
+++ src/msnCupon.js
@@ -0,0 +1,26 @@
+export const msn = {
+    mickey:{
+        mensaje:"¡Meeska, Mooska! Con Mickey ganas un 10% EXTRA en tu compra usando el cupón:",
+        cupon:"10MOUSE"
+    },
+    minnie:{
+        mensaje:"¡Meeska, Mooska! Con Minnie ganas ENVÍO GRATIS en tu compra usando el cupón:",
+        cupon:"MINNIESHIP"
+    },
+    donald:{
+        mensaje:"¡Cuak! Con Donald ganas 5% EXTRA en tu compra usando el cupón:",
+        cupon:"DONALD5"
+    },
+    pluto:{
+        mensaje:"Goofy no trae regalos para ti. Intenta armar otro personaje ¡Te esperan grandes sorpresas!",
+        cupon:""
+    },
+    nothing:{
+        mensaje:"!Que mal 😐 Vuelve a intentarlo!",
+        cupon:""
+    }
+}
+
+
+
+
